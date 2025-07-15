741cca72a2e6bbeba9424f00ad65f4f0e6181b0d
null0218 <imengying@outlook.com>
2025-07-16 00:31:23 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -59,9 +59,9 @@ xray() {
         "network": "tcp",
         "security": "reality",
         "realitySettings": {
-          "dest": "updates.cdn-apple.com",
+          "dest": "www.ua.edu",
           "serverNames": [
-            "updates.cdn-apple.com"
+            "www.ua.edu"
           ],
           "privateKey": "${PrivateKey}",
           "shortIds": [
@@ -108,7 +108,7 @@ EOF
     cat << EOF > /usr/local/etc/xray/config.txt
 
 vless-tcp-reality
-vless://${uuid}@${HOST_IP}:${PORT1}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=updates.cdn-apple.com&fp=chrome&pbk=${PublicKey}&sid=${shid}&type=tcp&headerType=none#${IP_COUNTRY}
+vless://${uuid}@${HOST_IP}:${PORT1}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.ua.edu&fp=chrome&pbk=${PublicKey}&sid=${shid}&type=tcp&headerType=none#${IP_COUNTRY}
 EOF
 
     echo "Xray 安装完成"
